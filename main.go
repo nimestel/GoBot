8ef3dbef2d8b452275ce649235395ad7c1dc8907
Tatiana <cherv.tatiana@gmail.com>
2020-08-13 20:37:37 +0300

    basic syntax

--- main.go
+++ main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	var test bool
+
+	text := "Hello world!"
+	fmt.Println(text, test)
+	fmt.Println(text + "2")
+
+	text = text + "123"
+	fmt.Println(text)
+
+	result := isAllowedGroups(2, 10)
+	fmt.Println("isAllowedGroups", result)
+}
+
+func isAllowedGroups(groupCount int, subscriberCount int) bool {
+	residue := subscriberCount % groupCount
+
+	fmt.Println(residue)
+
+	return residue == 0
 }
