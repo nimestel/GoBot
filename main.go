0fa41b786d30ad824003d15cf6250f9ea733c7db
Tatiana <cherv.tatiana@gmail.com>
2020-08-16 14:08:38 +0300

    method get updates

--- main.go
+++ main.go
@@ -20,24 +20,87 @@ type GetMeResultT struct {
 	Username  string `json:"username"`
 }
 
+type SendMessageT struct {
+	Ok     bool         `json:"ok"`
+	Result GetMeResultT `json:"result"`
+}
+
+type GetUpdatesT struct {
+	Ok     bool                `json:"ok"`
+	Result []GetUpdatesResultT `json:"result"`
+}
+type GetUpdatesMessageT struct {
+	MessageID int `json:"message_id"`
+	From      struct {
+		ID           int    `json:"id"`
+		IsBot        bool   `json:"is_bot"`
+		FirstName    string `json:"first_name"`
+		Username     string `json:"username"`
+		LanguageCode string `json:"language_code"`
+	} `json:"from"`
+	Chat struct {
+		ID        int    `json:"id"`
+		FirstName string `json:"first_name"`
+		Username  string `json:"username"`
+		Type      string `json:"type"`
+	} `json:"chat"`
+	Date int    `json:"date"`
+	Text string `json:"text"`
+}
+
+type GetUpdatesResultT struct {
+	UpdateId int                `json:"update_id"`
+	Message  GetUpdatesMessageT `json:"message,omitempty"`
+}
+
+type MessageT struct {
+	MessageId int                          `json:"message_id"`
+	From      GetUpdatesResultMessageFromT `json:"from"`
+	Chat      GetUpdatesResultMessageChatT `json:"chat"`
+	Date      int                          `json:"date"`
+	Text      bool                         `json:"text"`
+}
+
+type GetUpdatesResultMessageFromT struct {
+	Id           int    `json:"id"`
+	IsBot        bool   `json:"is_bot"`
+	FirstName    string `json:"first_name"`
+	Username     string `json:"username"`
+	LanguageCode string `json:"language_code"`
+}
+
+type GetUpdatesResultMessageChatT struct {
+	Id        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	Username  string `json:"username"`
+	Type      string `json:"type"`
+}
+
 const telegramBaseUrl = "https://api.telegram.org/bot"
 const telegramToken = "1397587106:AAF8socYPHdzdwbe6FuYW0M0GZicYlyGkLg"
 
 const methodGetMe = "getMe"
+const methodGetUpdates = "getUpdates"
 
 func main() {
 
-	fmt.Println(getUrlByMethod(methodGetMe))
+	//fmt.Println(getUrlByMethod(methodGetMe))
 
-	body := getBodyByUrlAndData(getUrlByMethod(methodGetMe), []byte(""))
+	body := getBodyByUrlAndData(getUrlByMethod(methodGetUpdates), []byte(""))
+
 	//fmt.Printf("%s", body)
-	getMe := GetMeT{}
-	err := json.Unmarshal(body, &getMe)
+
+	getUpdates := GetUpdatesT{}
+	err := json.Unmarshal(body, &getUpdates)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
-	fmt.Printf("%v", getMe)
+	for _, item := range getUpdates.Result {
+		fmt.Println(item.Message.Text)
+	}
+
+	//fmt.Printf("%v", getUpdates)
 }
 
 func getUrlByMethod(methodName string) string {
